b075b44d14086498cd9d60539e3a58c0ccaeadca
Catalina uwu <kellysugarcoat@gmail.com>
2021-04-15 22:21:21 -0300

    [ADD] vuejs

---
+++ gui/src/App.vue
@@ -0,0 +1,39 @@
+/* eslint-disable */
+<template>
+  <div id="app">
+    <button>google</button>
+    {{name}}
+    <input type="text" v-model="name">
+    <MyButton :text='name'>
+    </MyButton>
+    <MyButton text='owo'>
+    </MyButton>
+  </div>
+</template>
+
+<script>
+import MyButton from './components/MyButton.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyButton,
+  },
+  data: function(){
+    return {
+      name: ''
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ gui/src/components/MyButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style='background-color:purple'>
+      <div class='miboton'>
+        {{ text }}
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['text']
+}
+</script>
+
+<style>
+.miboton {
+    color: white;
+    padding: 10px
+}
+</style>
